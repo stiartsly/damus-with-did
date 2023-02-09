2541f80672dccc49ff0df00964d18511bfa94802
liaihong <>
2023-02-09 10:41:18 +0800

    add new did UI

--- damus/Views/CarouselView.swift
+++ damus/Views/CarouselView.swift
@@ -24,6 +24,14 @@ let carousel_items = [
                  text: Text("\(Text("安全", comment: "您的数据受到高度保护.").bold()). 您的数据受到高度保护.", comment: "Explanation of what can be done by users to earn money. There is a heading that precedes this explanation which is a variable to this string."))
 ]
 
+let createDID_items = [
+    CarouselItem(image: Image("Feeds-logo-signin"), text: Text("欢迎来到我的第一个身份.", comment: "注释")),
+    CarouselItem(image: Image("Feeds-logo-signin"),
+                 text: Text("此应用程序使用去中心身份（DID）。 使用去中心身份，您拥有自己的身份和数据。\n \n 因此，您似乎还不知道这是什么，或者您从未创建自己的身份？ 我们在这里为您提供帮助，以下步骤将自动为您创建和发布全新的Elastos身份和存储空间。", comment: "").font(.system(size: 18))),
+    CarouselItem(image: Image("Feeds-logo-signin"),
+                 text: Text("将来，如果您想更好地控制或在其他支持DID的应用程序中使用此身份，可以将其导出到第三方钱包应用程序，例如Elastos Essential.", comment: "Explanation of what is done to keep personally identifiable information private. There is a heading that precedes this explanation which is a variable to this string.").font(.system(size: 18))),
+]
+
 struct CarouselView: View {
         
     var body: some View {
@@ -47,10 +55,10 @@ struct CarouselNewView: View {
     var body: some View {
         
         TabView(selection: $selectedIndex) {
-            ForEach(0..<carousel_items.count) { i in
-                CarouselItemView(item: carousel_items[i])
+            ForEach(0..<createDID_items.count) { i in
+                CarouselItemView(item: createDID_items[i])
                     .tabItem {
-                        Text(carousel_items[i].id)
+                        Text(createDID_items[i].id)
                     }.tag(i)
             }
         }.tabViewStyle(PageTabViewStyle())
