5f9e1a91363704fc3b18685c02ef225b81872e10
liaihong <>
2023-02-14 21:26:46 +0800

    add create new did

--- damus/Util/DamusIdentity.swift
+++ damus/Util/DamusIdentity.swift
@@ -9,46 +9,96 @@ struct defaultsKeys {
 }
 let notApproveNetWork = "目前不允许数据连接。"
 let notNetWorkError = "无法连接网络：请点击登录重试"
+
 public class DamusIdentity {
     
     public var mnemonic: String = ""
     public var publicKey: String = ""
     public var rootIdentity: RootIdentity?
+    public var didStore: DIDStore?
+
     public var didString: String = ""
     public var rootPath: String = ""
     private var defaultStorePass: String = "DUMASDIDPASSWORD"
     private static var instance: DamusIdentity?
 
+    deinit {
+        rootIdentity = nil
+        didStore = nil
+        DamusIdentity.instance = nil
+    }
+    
     public class func shared() -> DamusIdentity {
         if (instance == nil) {
             instance = DamusIdentity()
+            let currentNet = "mainnet"
+            if (!DIDBackend.isInitialized()) {
+                do {
+                    try DIDBackend.initialize(DefaultDIDAdapter("https://api.elastos.io/eid"))
+                } catch {
+                    print("error =\(error)")
+                }
+            }
+            print("DIDBackend.initialize")
         }
         
         return instance!
     }
     
-    public func handleDidPkSk(mnemonic: String) -> String {
+    func createDidStore() throws {
+        let currentPath = Int.random(in: 0...1000)
+        rootPath = "\(NSHomeDirectory())/Library/Caches/DumausDIDStore" + "\(currentPath)"
+        didStore = try DIDStore.open(atPath: rootPath)
+        print("rootPath = \(rootPath)")
+    }
+    
+    public func createNewMnemonic(name: String) throws {
+        mnemonic = try Mnemonic.generate(Mnemonic.DID_ENGLISH)
+        rootIdentity = try RootIdentity.create(mnemonic, true, didStore!, defaultStorePass)
+    }
+    
+    public func createNewDid(name: String) throws {
+        try createNewMnemonic(name: name)
+        var doc = try rootIdentity!.newDid(defaultStorePass)
+        let db = try doc.editing()
+        let js = ["name": name]
+        let json = js.toJsonString() != nil ? js.toJsonString()! : ""
+        print("createNewDid json === \(json)")
+//        var json = "{\"name\":\"Foo Bar\"}"
+        _ = try db.appendCredential(with: "#name", json: json, using: defaultStorePass)
+        doc = try db.seal(using: defaultStorePass)
+        try doc.publish(using: defaultStorePass)
+        didString = doc.subject.description
+        print("createNewDid didString === \(didString)")
+    }
+    
+    public func createIdentity(mnemonic: String) throws {
+        // Generate a random password
+        if try !(didStore!.containsRootIdentities()) {
+            self.rootIdentity = try RootIdentity.create(mnemonic, false, didStore!, defaultStorePass)
+        }
+        self.rootIdentity = try didStore!.loadRootIdentity()
+        print("rootIdentity = \(self.rootIdentity)")
+    }
+    
+    public func handleMnemonic(mnemonic: String) -> String {
         do {
-            let currentPath = Int.random(in: 0...1000)
-            rootPath = "\(NSHomeDirectory())/Library/Caches/DumausDIDStore" + "\(currentPath)"
-            let didStore = try DIDStore.open(atPath: rootPath)
-            print("rootPath = \(rootPath)")
-            
-            let currentNet = "mainnet"
-            if (!DIDBackend.isInitialized()) {
-                try DIDBackend.initialize(DefaultDIDAdapter("https://api.elastos.io/eid"))
-            }
-            print("DIDBackend.initialize")
-            
-            // Generate a random password
-            if try !(didStore.containsRootIdentities()) {
-                self.rootIdentity = try RootIdentity.create(mnemonic, false, didStore, self.defaultStorePass)
-            }
-            self.rootIdentity = try didStore.loadRootIdentity()
-            print("rootIdentity = \(self.rootIdentity)")
-            
-            try didStore.synchronize()
-            let dids = try didStore.listDids()
+//            let currentPath = Int.random(in: 0...1000)
+//            rootPath = "\(NSHomeDirectory())/Library/Caches/DumausDIDStore" + "\(currentPath)"
+//            let didStore = try DIDStore.open(atPath: rootPath)
+//            print("rootPath = \(rootPath)")
+//
+//            // Generate a random password
+//            if try !(didStore.containsRootIdentities()) {
+//                self.rootIdentity = try RootIdentity.create(mnemonic, false, didStore, self.defaultStorePass)
+//            }
+//            self.rootIdentity = try didStore.loadRootIdentity()
+//            print("rootIdentity = \(self.rootIdentity)")
+//
+            try createDidStore()
+            try createIdentity(mnemonic: mnemonic)
+            try didStore!.synchronize()
+            let dids = try didStore!.listDids()
             print("dids = ", dids)
             
             //TODO: 默认只取第一个did // 其他的did暂时不支持
